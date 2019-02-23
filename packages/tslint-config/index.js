41f4439dc9c9c99d006357da66b58990c4bfbee9
James Newell <james@digitaledgeit.com.au>
2019-02-23 22:47:23 +1100

    disable .only() in tests

--- packages/tslint-config/index.js
+++ packages/tslint-config/index.js
@@ -1,3 +1,23 @@
 module.exports = {
+  "extends": ["tslint:recommended", "tslint-config-prettier"],
+  "rules": {
+    "interface-name": false,
+    "ordered-imports": [false],
+    "object-literal-sort-keys": [false],
+    "quotemark": [false],
 
+    "ban": [
+      true,
+
+      // disable .only() in tests
+      { "name": ["fdescribe"], "message": "Don't focus tests!" },
+      { "name": ["describe", "only"], "message": "Don't focus tests!" },
+      { "name": ["describe", "skip"], "message": "Don't focus tests!" },
+      { "name": ["fit"], "message": "Don't focus tests!" },
+      { "name": ["it", "only"], "message": "Don't focus tests!" },
+      { "name": ["test", "only"], "message": "Don't focus tests!" },
+      { "name": ["it", "skip"], "message": "Don't focus tests!" }
+    ]
+
+  }
 };

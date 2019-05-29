9b5e7b081f6611446e60331e19a0a6f400177392
James Newell <james@digitaledgeit.com.au>
2019-05-29 19:28:45 +1000

    added support for react/jsx

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,17 +1,34 @@
-
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint', 
-    'prettier'
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "react"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint"
   ],
   rules: {
-    "no-var": "error"
+    // note you must disable the base rule as it can report incorrect errors
+    // @see https://github.com/bradzacher/eslint-plugin-typescript/blob/master/docs/rules/no-unused-vars.md#options
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      }
+    ]
   }
 };

bbe94f18e02791b8571e01036c6c80f76b6df9cd
James Newell <james@digitaledgeit.com.au>
2019-06-05 21:20:26 +1000

    switch from babel to ts-jest

--- packages/jest-preset-test/jest-preset.js
+++ packages/jest-preset-test/jest-preset.js
@@ -1,10 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const {defaults} = require('jest-config');
-
 module.exports = {
+  preset: 'ts-jest',
   testMatch: ['<rootDir>/(src|test)/**/*.test.ts?(x)'],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  transform: {
-    '^.+\\.tsx?$': require.resolve('babel-jest')
-  }
 };

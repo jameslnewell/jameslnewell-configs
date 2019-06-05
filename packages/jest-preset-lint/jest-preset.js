4036829007701f70c9a12f6ec4a30c140d17ec7f
James Newell <james@digitaledgeit.com.au>
2019-06-05 21:19:17 +1000

    added watch plugin

--- packages/jest-preset-lint/jest-preset.js
+++ packages/jest-preset-lint/jest-preset.js
@@ -4,5 +4,6 @@ const {defaults} = require('jest-config');
 module.exports = {
   runner: require.resolve('jest-runner-eslint'),
   testMatch: ['<rootDir>/(src|test)/**/*.ts?(x)'],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"]
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  watchPlugins: ['jest-runner-eslint/watch-fix'],
 };

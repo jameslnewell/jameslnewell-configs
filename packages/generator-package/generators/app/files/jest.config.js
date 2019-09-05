c4816dffe5a3e09068fec29c72ed1456937629fd
James Newell <james@jameslnewell.dev>
2019-09-05 21:24:56 +1000

    got the generator working

--- packages/generator-package/generators/app/files/jest.config.js
+++ packages/generator-package/generators/app/files/jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   projects: [
     {
-      displayName: 'lint',
-      preset: '@jameslnewell/jest-preset-lint',
-    },
-    {
-      displayName: 'type',
-      preset: '@jameslnewell/jest-preset-type',
-    },
-    {
       displayName: 'test',
       preset: '@jameslnewell/jest-preset-test',
     },
--- packages/generator-package/generators/app/files/src/index.test.ts
+++ packages/generator-package/generators/app/files/src/index.test.ts
@@ -1,4 +1,4 @@
-import {sum} from './index';
+import {sum} from '.';
 
 it('should add two numbers', () => {
   expect(sum(1, 2)).toEqual(3);

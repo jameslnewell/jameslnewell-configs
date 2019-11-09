dfb170f3ef2741b8c1fb77ee09f1c94392f303a5
James Newell <james.newell@automattic.com>
2019-11-10 03:48:59 +1100

    support JSON in module resolvers

--- packages/jest-preset-test/jest-preset.js
+++ packages/jest-preset-test/jest-preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: ['<rootDir>/(src|test)/**/*.test?(s).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': require.resolve('babel-jest'),
   },
